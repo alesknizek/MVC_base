bf5f22dd880ac54f9980e7c59a8d8fb0119c8d58
Aleš Knížek <knizek@spstrutnov.cz>
2022-04-19 13:56:00 +0200

    snad závěrečné změny

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # MVC_base
 
 základ aplikace podle vzoru MVC
+
+příkazy:
+    npm config set proxy http://proxy.skola.local:3128 #nastavení proxy 
+    npm init #inicializace projektu, pro použití v novém projektu použít znovu
+    npm install <module> #instalace modulu
+
+.dev
+    HOST=localhost
+    PORT=8000
--- src/app/routers/indexRouter.js
+++ src/app/routers/indexRouter.js
@@ -6,17 +6,7 @@ const router = express.Router();
 
 const controller = require(path.join(__dirname, '..', 'controllers', 'controller'));
 
-/*
-    všechny rozpoznávané URL budou zachytávat vaše routery, např.
-
-    router.use('/uzivatele', require(path.join(__dirname, 'uzivateleRouter')));
-    router.use('/hry', require(path.join(__dirname, 'hryRouter')));
-
-    apod.
-*/
-
 router.use('/Main', require(path.join(__dirname, 'router')));
-//router.use('/Admin', require(path.join(__dirname, 'AdminRouter')));
 
 router.get([
     '/',
